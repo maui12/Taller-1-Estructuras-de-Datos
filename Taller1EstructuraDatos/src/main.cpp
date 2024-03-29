de74ba0f6387ba840247024c1456d9e980972546
maui12 <103455248+maui12@users.noreply.github.com>
2024-03-28 22:57:56 -0300

    nuevas clases

---
+++ Taller1EstructuraDatos/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Asistente.h"
+
+using namespace std;
+
+int main() {
+    Asistente* a1 = new Asistente("mauricio",21);
+
+    cout<<a1->getNombre()<<endl;
+    cout<<a1->getEdad()<<endl;
+
+    return 0;
+}
