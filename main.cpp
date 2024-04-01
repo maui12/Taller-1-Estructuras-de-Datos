2b0ea47075f27f2d191108ed1642de2035c2cf83
maui12 <103455248+maui12@users.noreply.github.com>
2024-04-01 20:45:37 -0300

    menu

--- main.cpp
+++ main.cpp
@@ -3,13 +3,53 @@
 
 using namespace std;
 
+bool menu() {
+   	int entrada = 0;
+
+   	cout<<"MENU"<<endl;
+   	cout<<"1.- Crear nuevo evento"<<endl;
+   	cout<<"2.- Registrar nuevo Asistente"<<endl;
+   	cout<<"3.- Consultar lista de Asistentes"<<endl;
+   	cout<<"4.- Generar Informe"<<endl;
+   	cout<<"5.- Guardar cambios y salir"<<endl;
+   	cout<<"-Ingrese una opcion: "<<endl;
+   	cin>>entrada;
+
+   	switch(entrada) {
+
+   	case 1:
+
+   		break;
+   	case 2:
+
+   		break;
+   	case 3:
+
+   		break;
+   	case 4:
+
+   		break;
+   	case 5:
+
+   		return false;
+
+   	default:
+   		cout<<"Opcion invalida. ingrese otra opcion "<<endl;
+   		break;
+   	}
+
+
+   	return true;
+   }
+
 int main() {
-    Asistente* a1 = new Asistente("mauricio",21);
 
-    cout<<a1->getNombre()<<endl;
-    cout<<a1->getEdad()<<endl;
-    cout<<&a1<<"chaochao"<<endl;
+	bool salir = true;
 
+	do {
+		salir = menu();
+	}
+	while(salir);
 
     return 0;
 }
