1ac6f80890b7faad6204cbf6af9aad9bf339b7cb
CarlosLealS <103975174+CarlosLealS@users.noreply.github.com>
2024-04-02 23:57:58 -0300

    profesional added

--- Profesional.cpp
+++ Profesional.cpp
@@ -5,5 +5,11 @@
 using namespace std;
 
 class Profesional::Asistente(string nombre,int edad,string ocupacion):Asistente(nombre,edad){
-
-};
+    this -> ocupacion = ocupacion;
+}
+string Profesional::getOcupacion(){
+    return ocupacion;
+}
+void Profesional::setOcupacion(string ocupacion){
+    this -> ocupacion = ocupacion;
+}
