e4c7a2643bce30078dd510e65ce407755119ee19
maui12 <103455248+maui12@users.noreply.github.com>
2024-04-04 11:31:20 -0300

    first option menu

--- Sistema.cpp
+++ Sistema.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "Sistema.h"
 #include "Evento.h"
-#include "Asistente.h"
+#include "Asistente.h"¨
+#include "Conferencia.h"
 
 using namespace std;
 
@@ -11,12 +12,36 @@ Sistema::Sistema() {
 }
 
 void Sistema::CrearEvento() {
-	cout<<"=====Crear un nuevo Evento====="<<endl;
+	cout<<"=====Crear un nuevo Evento===== \nElija el tipo de evento: \n1)Conferencia 2)Concierto 3)Taller"<<endl;
+	int tipo;
+	cin >> tipo;
+
+	if(tipo != 1 && tipo != 2 && tipo != 3) {
+		cout<<"Dato invalido";
+		return;
+	}
+
 	cout<<"Ingrese nombre del nuevo evento: "<<endl;
 	string nombre;
 	cin >> nombre;
 
-	Evento* evento = new Evento(nombre);
+	string atributo;
+	Evento* evento;
+
+	switch(tipo) {
+	case 1:
+		cout<<"Conferencia: señale el tema: "<<endl;
+		cin >> atributo;
+
+		break;
+	case 2:
+
+		break;
+	case 3:
+
+		break;
+
+	}
 
 	eventos.push_front(evento);
 
